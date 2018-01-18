0cc3d2a9b92c9edc06e4103908b6ba0a051954a7
Juerg Haefliger <juergh@gmail.com>
2018-01-18 15:55:41 +0100

    Add cdc-echo example
    Signed-off-by: Juerg Haefliger <juergh@gmail.com>

---
+++ cdc-echo/cdc-echo.ino
@@ -0,0 +1,32 @@
+/* -*- mode: linux-c -*- */
+
+#include <Arduino.h>
+#include <DigiCDC.h>
+
+#define PIN_LED PB1
+
+void setup() {         
+	SerialUSB.begin(); 
+
+	/* Initialize the digital pin as an output and set it */
+	DDRB |= (1 << PIN_LED);
+	PORTB |= (1 << PIN_LED);
+}
+
+void loop() {
+	int i;
+	
+	if (SerialUSB.available()) {
+		SerialUSB.write(SerialUSB.read());
+	} else {
+		/*
+		 * If you don't call a SerialUSB function (write, print, read,
+		 * available, etc) every 10ms or less then you must throw in
+		 * some SerialUSB.refresh(); for the USB to keep alive - also
+		 * replace your delays - ie. delay(100); - with
+		 * SerialUSB.delays ie. SerialUSB.delay(100);
+		 */
+		SerialUSB.delay(5);
+		SerialUSB.refresh();
+	}
+}
